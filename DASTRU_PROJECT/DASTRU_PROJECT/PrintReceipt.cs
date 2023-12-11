700245ac82a6038d1d14e3c4d91d6ea2e3c3e15f
brickshow <127967015+brickshow@users.noreply.github.com>
2023-12-11 10:46:55 +0800

    Add files via upload

---
+++ DASTRU_PROJECT/DASTRU_PROJECT/PrintReceipt.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Alcuino.ConsoleWriter472;
+
+namespace DASTRU_PROJECT
+{
+    internal class PrintReceipt
+    {
+        string items { get; set; }
+
+        public static void printItems(string items, double price, double CASH)
+        {
+            Console.CursorVisible = true;
+            ConsoleWriter.WriteHeader("DON MACCHIATOS", ConsoleColor.Cyan, 100);
+            
+            
+            Console.WriteLine("Priting receipt....\n");
+            string printHeader = "             DON MACCHIATOS            ";
+            string printLine = "---------------------------------------";
+
+            //Print the header using substring
+            for (int i = 0; i < printHeader.Length; i++)
+            { 
+                Console.Write(printHeader.Substring(0, i + 1));
+                Thread.Sleep(50);
+                Console.SetCursorPosition(Console.CursorLeft - (i + 1), Console.CursorTop);
+            }
+            Console.WriteLine("\n");
+
+
+            //Print the line using substring
+            for (int i = 0; i < printLine.Length; i++)
+            {
+                Console.Write(printLine.Substring(0, i + 1));
+                Thread.Sleep(50);
+                Console.SetCursorPosition(Console.CursorLeft - (i + 1), Console.CursorTop);
+            }
+            Console.WriteLine();
+            
+            //Print the items using substring
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.Write(items.Substring(0, i + 1));
+                Thread.Sleep(50);
+                Console.SetCursorPosition(Console.CursorLeft - (i + 1), Console.CursorTop);
+            }
+
+            string printPrice = price.ToString();
+            //Print the price using substring
+            Console.SetCursorPosition(35, 8);
+            for (int i = 0; i < printPrice.Length; i++)
+            {
+                Console.Write(printPrice.Substring(0, i + 1));
+                Thread.Sleep(50);
+                Console.SetCursorPosition(Console.CursorLeft - (i + 1), Console.CursorTop);
+            }
+            Console.WriteLine();
+
+            //Print the line using substring
+            for (int i = 0; i < printLine.Length; i++)
+            {
+                Console.Write(printLine.Substring(0, i + 1));
+                Thread.Sleep(50);
+                Console.SetCursorPosition(Console.CursorLeft - (i + 1), Console.CursorTop);
+            }
+            Console.WriteLine("\n\n");
+
+            //Display the total price
+            string totatAmount = "Total Amount                      P " + printPrice;
+            for (int i = 0; i < totatAmount.Length; i++)
+            {
+                Console.Write(totatAmount.Substring(0, i + 1));
+                Thread.Sleep(50);
+                Console.SetCursorPosition(Console.CursorLeft - (i + 1), Console.CursorTop);
+            }
+            Console.WriteLine();
+
+            //Print total price
+            string totatCash = "Total Cash                        P " + CASH.ToString();
+            for (int i = 0; i < totatCash.Length; i++)
+            {
+                Console.Write(totatCash.Substring(0, i + 1));
+                Thread.Sleep(50);
+                Console.SetCursorPosition(Console.CursorLeft - (i + 1), Console.CursorTop);
+            }
+            Console.WriteLine();
+
+            //Print total price
+            CASH -= price;
+            string Change = "Change                            P " + CASH.ToString();
+            for (int i = 0; i < Change.Length; i++)
+            {
+                Console.Write(Change.Substring(0, i + 1));
+                Thread.Sleep(50);
+                Console.SetCursorPosition(Console.CursorLeft - (i + 1), Console.CursorTop);
+            }
+
+        }
+    }
+}
---
+++ DASTRU_PROJECT/DASTRU_PROJECT/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Threading;
+using Alcuino.ConsoleWriter472;
+using DASTRU_PROJECT;
+
+namespace DASTRU_FINAL_PROJECT
+{
+    class Program
+    {
+
+
+        public static object Color { get; private set; }
+
+        static string[] products =
+         { "Don Darko", "Donya Berry", "Don Matchatos", "Caramel Macchiato", "Iced Mocha",
+          "Iced Ube", "Iced Chocolate", "Cookie Dough Iced Latte", "Pumpkin Spice Latte", "Spanish Latte" };
+
+        static double[] price = { 39.00, 39.00, 49.00, 59.00, 39.00, 39.00, 49.00, 59.00, 39.00, 39.00 };
+
+        static LinkedList<string> orderedItems = new LinkedList<string>();
+        static int selectedItem;
+        static void Main(string[] args)
+        {
+           
+        again:
+            ConsoleWriter.WriteHeader("DON MACCHIATOS", ConsoleColor.Cyan, 100);
+            double totalPrice = 0;
+            string yn;
+
+            string productSelected = ProductSelection();
+            Console.Clear();
+            double quantity = SelectQuantity();
+            Console.Clear();
+
+            totalPrice += quantity * price[selectedItem];
+            string productDetail = productSelected + " " + quantity + " pcs: " + (quantity * price[selectedItem]) + " pesos";
+
+            orderedItems.AddLast(productDetail);
+            Console.WriteLine(productDetail);
+
+            Console.WriteLine("Successfully added.\n[F1] Order again? \t [F2] Checkout? "); Console.CursorVisible = false;
+            yn = Console.ReadKey().KeyChar.ToString();
+
+            ConsoleKeyInfo consoleKey = Console.ReadKey();
+           if (consoleKey.Key == ConsoleKey.F1)
+           {
+                orderedItems.Clear(); Console.Clear();
+                goto again;
+           }
+           else if (consoleKey.Key == ConsoleKey.F2)
+           {
+                Console.WriteLine("All items to be checked out\n");
+                Console.Clear();
+                foreach (string item in orderedItems)
+                { Console.WriteLine(item); }
+
+                Console.WriteLine("\nTotal: " + totalPrice + " pesos");
+                Console.WriteLine("Successfully checkd out.");
+            cashEnter:
+                Console.CursorVisible=true;
+                Console.Write("\nPlease enter cash: ");
+                var cash = Convert.ToDouble(Console.ReadLine());
+
+                if (cash < totalPrice) { Console.WriteLine("Insufficient Cash", ConsoleColor.Red); Console.ReadKey(); goto cashEnter; }
+                else
+                {
+                    PrintReceipt.printItems(productSelected, totalPrice, cash);
+                    Console.WriteLine("\n\nClick [Enter] to continue");
+                    Console.ReadKey();
+                    goto again;
+                }
+
+                
+            }
+        }   
+
+        static string ProductSelection()
+        {
+            selectedItem = 0;
+            Console.WriteLine("List of products:");
+            foreach (string item in products)
+            { Console.WriteLine("-> " + item); }
+            Console.Write("Choose product: ");
+            string ans = Console.ReadLine().TrimEnd().TrimStart().ToLower();
+
+            if (ans.Length == 0)
+            { Console.Clear(); Console.WriteLine("Invalid, please try again."); return ProductSelection(); }
+            else
+            {
+                foreach (string item in products)
+                {
+                    if (item.ToLower() == ans)
+                    { return ans; }
+                    selectedItem++;
+                }
+                Console.Clear(); Console.WriteLine("Invalid, please try again."); return ProductSelection();
+            }
+        }
+
+        static double SelectQuantity()
+        {
+         
+            Console.Write("Enter quantity: ");
+            if (!int.TryParse(Console.ReadLine(), out int quantity) || quantity <= 0)
+            { Console.Clear(); Console.WriteLine("Invalid, please try again."); return SelectQuantity(); }
+            else { return quantity; }
+        }
+    }
+}
